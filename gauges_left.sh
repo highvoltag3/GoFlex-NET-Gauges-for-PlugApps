aeeca9b3554cb52aaf35e08b0c15b3a603fd2647
Dario J Novoa V. <darionovoa@ideartte.com>
2011-11-03 22:47:24 -0700

    Fixed the wrong amount being displayed...

--- gauges_left.sh
+++ gauges_left.sh
@@ -23,43 +23,7 @@ PERCENT=`df -h|grep $DISC|awk '{print $5}'`
 # Let's remove the % from the Percent variable like this var=${var%\%}
 PERCENT=${PERCENT%\%}
 
-#When we hit 95% 972.8Mb or more flash the last 2 leds in a synchronized way to warn me I'm running out of space
-if [ $PERCENT -ge 95 ]
-then
-
-# 3 is the upper most led and 0 is the one on the bottom
-  echo default-on > /sys/class/leds/status\:white\:left0/trigger
-  echo default-on > /sys/class/leds/status\:white\:left1/trigger
-  echo timer > /sys/class/leds/status\:white\:left2/trigger
-  echo 1000 > /sys/class/leds/status\:white\:left2/delay_on
-  echo timer > /sys/class/leds/status\:white\:left3/trigger
-  echo 500 > /sys/class/leds/status\:white\:left3/delay_on
-  
-  echo "Your using an amount of disk space that is Greater or equal to 85%"
- 
-elif [ $PERCENT -le 75 ]
-then
-  echo default-on > /sys/class/leds/status\:white\:left0/trigger
-  echo default-on > /sys/class/leds/status\:white\:left1/trigger
-  echo default-on > /sys/class/leds/status\:white\:left2/trigger
-  
-  echo none > /sys/class/leds/status\:white\:left3/trigger
-
-  echo "Your using an amount of disk space that is less or equal to 75%"
-
-
-elif [ $PERCENT -le 50 ]
-then
-  echo default-on > /sys/class/leds/status\:white\:left0/trigger
-  echo default-on > /sys/class/leds/status\:white\:left1/trigger
-  
-  echo none > /sys/class/leds/status\:white\:left2/trigger
-  echo none > /sys/class/leds/status\:white\:left3/trigger
-
-  echo "Your using an amount of disk space that is less or equal to 50%"
-
-
-elif [ $PERCENT -le 25 ]
+if [ $PERCENT -le 25 ]
 then 
   echo default-on > /sys/class/leds/status\:white\:left0/trigger
   
@@ -69,10 +33,57 @@ then
 
   echo "Your using an amount of disk space that is less or equal to 25%"
 
+elif [ $PERCENT -gt 25 -o $PERCENT -le 50 ]
+then
+  echo default-on > /sys/class/leds/status\:white\:left0/trigger
+  echo default-on > /sys/class/leds/status\:white\:left1/trigger
+  
+  echo none > /sys/class/leds/status\:white\:left2/trigger
+  echo none > /sys/class/leds/status\:white\:left3/trigger
+
+  echo "Your using an amount of disk space that is less or equal to 50%"
+
+elif [ $PERCENT -gt 50 -o $PERCENT -le 75 ]
+then
+  echo default-on > /sys/class/leds/status\:white\:left0/trigger
+  echo default-on > /sys/class/leds/status\:white\:left1/trigger
+  echo default-on > /sys/class/leds/status\:white\:left2/trigger
+  
+  echo none > /sys/class/leds/status\:white\:left3/trigger
+
+  echo "Your using an amount of disk space that is less or equal to 75%"
+
+elif [ $PERCENT -gt 75 -o $PERCENT -le 95 ]
+then
+
+  echo default-on > /sys/class/leds/status\:white\:left0/trigger
+  echo default-on > /sys/class/leds/status\:white\:left1/trigger
+  echo default-on > /sys/class/leds/status\:white\:left2/trigger
+  
+  echo none > /sys/class/leds/status\:white\:left3/trigger
+
+  echo "Your using an amount of disk space that is greater than 75 but less than 95%"
+
+#When we hit 95% 972.8Mb or more flash the last 2 leds in a synchronized way to warn me Im running out of space
+elif [ $PERCENT -gt 95 -o $PERCENT -le 100 ]
+then
+
+#3 is the upper most led and 0 is the one on the bottom
+  echo default-on > /sys/class/leds/status\:white\:left0/trigger
+  echo default-on > /sys/class/leds/status\:white\:left1/trigger
+  echo timer > /sys/class/leds/status\:white\:left2/trigger
+  echo 1000 > /sys/class/leds/status\:white\:left2/delay_on
+  echo timer > /sys/class/leds/status\:white\:left3/trigger
+  echo 500 > /sys/class/leds/status\:white\:left3/delay_on
+  
+  echo "Your using an amount of disk space that is Greater or equal to 95%"
 
 else
+
+#We got no idea so let's turn all off
   echo none > /sys/class/leds/status\:white\:left0/trigger
   echo none > /sys/class/leds/status\:white\:left1/trigger
   echo none > /sys/class/leds/status\:white\:left2/trigger
   echo none > /sys/class/leds/status\:white\:left3/trigger
+
 fi
